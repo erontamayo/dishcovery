89203b1ec02e0fbf50639671eeddc882146aebe3
erontamayo <erontamayo@gmail.com>
2026-06-23 12:13:40 +0800

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -5,14 +5,9 @@ const nextConfig = {
   },
   images: {
     remotePatterns: [
-      {
-        protocol: 'https',
-        hostname: 'images.unsplash.com',
-      },
-      {
-        protocol: 'https',
-        hostname: 'plus.unsplash.com',
-      }
+      { protocol: 'https', hostname: 'images.unsplash.com' },
+      { protocol: 'https', hostname: 'plus.unsplash.com' },
+      { protocol: 'https', hostname: 's3.us-west-2.amazonaws.com' },
     ]
   }
 }
